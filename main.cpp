802e18f7e1e1a2991faddb128fbb514c96dd6536
NatalySv <136372155+NatalySv@users.noreply.github.com>
2023-06-18 22:36:09 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include <QCoreApplication>
 #include <iostream>
 
-int fact1(int x)
+int fact(int x)
 {
     int result;
     result = 1;
@@ -13,33 +13,20 @@ int fact1(int x)
     return result;
 }
 
-
-int fact2(int y)
-{
-    int result;
-    result = 1;
-
-    for (int i = 1; i <= y; i++)
-    {
-        result = result * i;
-    }
-    return result;
-}
-
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
 
     int n, m, num;
-    num = n - m;
     std::cout << "\nInput a natural number n <= 10:\n";
     std::cin >> n;
     std::cout << "\nInput a natural number m <= 10:\n";
     std::cin >> m;
+    num = n - m;
     if (n<=10 && m<=n)
     {
 
-        std::cout << "Number of accommodation options = " << fact1(n)/fact2(num);
+        std::cout << "Number of accommodation options = " << fact(n)/fact(num);
 
     }
     else {
